49b94700a0b23ed20d959fa8e296c2761e2be309
yuri-swift <yuri.swift.615@gmail.com>
2016-12-24 16:54:59 +0900

    commit

---
+++ Particle/SKScene.swift
@@ -0,0 +1,31 @@
+//
+//  SKScene.swift
+//  Particle
+//
+//  Created by 田山　由理 on 2016/12/24.
+//  Copyright © 2016年 Yuri Tayama. All rights reserved.
+//
+
+import SpriteKit
+
+class TestSKScene : SKScene {
+    
+    override func didMove(to view: SKView) {
+        self.backgroundColor = SKColor.clear
+        //little trick for iPad
+        self.scaleMode = .resizeFill
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(TestSKScene.emitParticle(_:)), name: NSNotification.Name(rawValue: "didPressMagicButton"), object: nil)
+    }
+    
+    func emitParticle(_ notification: Foundation.Notification) {
+        
+        guard let testParticlePath = Bundle.main.path(forResource: "Particle", ofType: "sks") else {
+            return
+        }
+        
+        let testParticle = NSKeyedUnarchiver.unarchiveObject(withFile: testParticlePath) as! SKEmitterNode
+        testParticle.position = CGPoint(x: self.frame.midX, y: self.frame.maxY)
+        self.addChild(testParticle)
+    }
+}
---
+++ Particle/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Particle
+//
+//  Created by 田山　由理 on 2016/12/24.
+//  Copyright © 2016年 Yuri Tayama. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import Spring
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var skView: SKView!
+    @IBOutlet weak var presentAnimView: SpringView!
+    @IBOutlet weak var magicButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let scene = TestSKScene(size: skView.bounds.size)
+        skView.presentScene(scene)
+    }
+    
+    @IBAction func didPressMagicButton(_ sender: AnyObject) {
+        
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "didPressMagicButton"), object: self)
+        self.presentAnimView.animate()
+        
+        var timer:Timer = Timer()
+        timer = Timer.scheduledTimer(timeInterval: 1.5,
+            target: self,
+            selector: #selector(ViewController.chageImage),
+            userInfo: nil,
+            repeats: false)
+        
+    }
+
+    func chageImage() {
+        let openBoxImg = UIImage(named: "presentBoxOpen")
+        self.magicButton.setImage(openBoxImg, for: .normal)
+        
+    }
+    
+}
+
